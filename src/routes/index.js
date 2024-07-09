92dd56218c9c034fab1796185ff0413e723b9943
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-07-09 11:00:24 +0700

    feat: student crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const adminRouter = require('../routes/admin.route');
 const courseRouter = require('../routes/course.route');
 const classRouter = require('../routes/class.route');
 const teacherRouter = require('../routes/teacher.route');
+const studentRouter = require('../routes/student.route');
 const testRouter = require("./test.route");
 function route(app) {
     app.use("/test", testRouter)
@@ -9,6 +10,7 @@ function route(app) {
     app.use("/admin/course", courseRouter)
     app.use("/admin/teacher", teacherRouter)
     app.use("/admin/class", classRouter)
+    app.use("/admin/student", studentRouter)
 
 }
 
