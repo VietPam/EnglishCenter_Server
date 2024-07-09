63c1e15117b0aa0eecd691417ac9f6c984732d68
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-07-09 09:54:00 +0700

    feat: crud class, add student, add teacher, set class finish

--- src/routes/class.route.js
+++ src/routes/class.route.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const classController = require('../controllers/class.controller');
 
 router.post('/create',classController.createClass)
+router.put('/update',classController.updateClass)
+router.post('/addstudent',classController.addStudentInClass)
+router.post('/addteacher',classController.addTeachersInClass)
 router.get('/listclass',classController.listClass)
-
+router.get('/finished',classController.finishedClass)
+router.get('/getclass',classController.getClass)
+router.get('/getClassesById',classController.getClassesById)
 
 module.exports = router;
